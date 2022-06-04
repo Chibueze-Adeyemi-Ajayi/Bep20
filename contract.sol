bfb784b66cb38ee0e0f7301490ea5fcdb24fd551
Jilo developer <70321034+Chibueze-Adeyemi-Ajayi@users.noreply.github.com>
2022-06-03 17:31:37 -0700

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -1,3 +1,7 @@
+/*
+* This code is subject to copy
+*/
+
 pragma solidity 0.5.16;
 
 interface IBEP20 {
@@ -29,7 +33,7 @@ interface IBEP20 {
   /**
    * @dev Returns the amount of tokens owned by `account`.
    */
-  function balanceOf(address account) external view returns (uint256);
+  function balanceOf(address account) external view returns (uint256); 
 
   /**
    * @dev Moves `amount` tokens from the caller's account to `recipient`.
